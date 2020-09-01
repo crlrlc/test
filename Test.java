c680fa2d8b594df87916224745651a24af6aa90d
crlrlc <70580090+crlrlc@users.noreply.github.com>
2020-09-01 21:33:34 +0800

    更换logo
    第一次用GitHub上传文件

---
+++ Test.java
@@ -0,0 +1,37 @@
+public class Test{
+	public static void main(String[] args){
+		//System.out.println("1");
+		
+		/*byte b = 106;
+		System.out.println(b);
+		
+		short s = 84;
+		System.out.println(s);
+		
+		int i = 122;
+		System.out.println(i);
+		
+		long l = 207;
+		System.out.println(l);
+		
+		float f = 1.25f;
+		System.out.println(f);
+		
+		double d = 1.66;
+		System.out.println(d);
+		
+		char c = 'a';
+		System.out.println(c);
+		
+		boolean b2 = true;
+		System.out.println(b2);*/
+		
+		//string s3 = "he" + "ll" + "o"//加起来拼接；
+		
+		String s3 = "he" + "ll" + "o" + " crl!!!!!!!!";//注意S的大小写！！！！！还有分号；
+		System.out.println(s3);
+		//注意S的大小写！！！！！还有分号；
+		
+		
+	}
+}
