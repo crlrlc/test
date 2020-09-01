904d2cc04b0d0c75ecfc5aec018e78fdcee6ccd8
crlrlc <70580090+crlrlc@users.noreply.github.com>
2020-09-01 21:16:32 +0800

    创建新文件
    第一次使用GitHub

---
+++ helloworld.java
@@ -0,0 +1,27 @@
+/**
+*文档注释
+*这个是打印xxxx的程序；
+*@author crl
+*@version xxx
+*/
+
+public class HelloWorld{
+            public static void main(String[] args){
+                      //（单行注释）打印HelloWorld
+                        System.out.println("*                                 *");
+                        System.out.println("                ");
+                        System.out.println("   *     *i love you*     *");
+                        System.out.println("                ");
+                        System.out.println("        *                *");
+                        System.out.println("      ");
+                        System.out.println("                   *");
+                       /*
+                        （多行注释）
+                        */
+                         //System.out.println("hello world");
+		/* System.out.println("Hello,crl!!!!!!!!!");
+		 System.out.println("Hello,crl!!!!!!!!!");
+		 System.out.println("Hello,crl!!!!!!!!!");*/
+
+
+             }
